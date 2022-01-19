b2fbd0499443aba1e7dfa53689fff4b34df11bc6
Benjamin Perkins <benperk@microsoft.com>
2022-01-19 12:19:52 +0100

    Update http.cs

--- csharpguitardevops/http.cs
+++ csharpguitardevops/http.cs
@@ -17,7 +17,7 @@ namespace csharpguitardevops
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request. #1");
 
             string name = req.Query["name"];
 
